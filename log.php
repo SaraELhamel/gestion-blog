176631dbcbc4699c2c2234befa90cebeb705d005
SaraELhamel <saraelhamel88@gmail.com>
2020-05-27 12:01:35 +0100

    new changes

--- log.php
+++ log.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 
     <title>Login page</title>
 </head>
+<h1>IT Blog</h1>
+<h3>Welcome Everyone</h3>
 <body>
     <div id=form>
 <form action="process.php" method="POST">
@@ -21,11 +24,9 @@
 <p>
     <label >Email</label>
     <input type="email" id=em name="em">
-    <input type="email" id="btn" value="Login"/>
+   
 </p>
-<p>
-    <input type="submit">
-</p>
+<input type="button" id="btn" value="Login"/>
 </form>
     </div>
 </body>
